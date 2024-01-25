cdb39eee2e306dfe88df9854d8cd417cbc508aa3
Pedro <pedro@cardexchangeid.com>
2024-01-25 15:52:23 +0000

    End of section 8

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent{
+
+
+  logo = "assets/images/logo.png";
+}
